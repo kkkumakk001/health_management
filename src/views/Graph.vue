2f8be2bcd127d6083bf5aeb2d9cf93c624091dd4
kkkumakk001 <kk.10.kuma.kk@gmail.com>
2022-11-12 18:12:24 +0900

    円グラフの記述場所の移行

--- src/views/Graph.vue
+++ src/views/Graph.vue
@@ -1,5 +1,17 @@
 <template>
   <div class="graph">
     <h1>This is an graph page</h1>
+    <DoughnutChart />
   </div>
 </template>
+
+<script>
+import DoughnutChart from '../components/ChartPie.vue'
+
+export default {
+  name: 'App',
+  components: {
+    DoughnutChart
+  }
+}
+</script>
