e5673c2eaccff467b433fa6c6b7cd564e8535702
kkkumakk001 <kk.10.kuma.kk@gmail.com>
2022-11-15 03:43:26 +0900

    ChartLineComponentの読み込み

--- src/views/Graph.vue
+++ src/views/Graph.vue
@@ -2,16 +2,19 @@
   <div class="graph">
     <h1>This is an graph page</h1>
     <DoughnutChart />
+    <LineChart />
   </div>
 </template>
 
 <script>
-import DoughnutChart from '../components/ChartPie.vue'
+import DoughnutChart from '../components/ChartPieComponent.vue'
+import LineChart from '../components/ChartLineComponent.vue'
 
 export default {
   name: 'App',
   components: {
-    DoughnutChart
+    DoughnutChart,
+    LineChart
   }
 }
 </script>
