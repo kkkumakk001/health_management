d6d54133744392fa785b88ab7e046b5c1c63f479
kkkumakk001 <kk.10.kuma.kk@gmail.com>
2022-11-17 20:02:26 +0900

    Githubアカウントへのリンクの設定

--- src/views/Config.vue
+++ src/views/Config.vue
@@ -2,6 +2,8 @@
   <div class="congif">
     <h1>This is an config page</h1>
     <h2>ログアウト機能</h2>
+    <p><a href="#">ここを押すとログアウト画面に遷移します</a></p>
     <h2>Githubアカウント連携</h2>
+    <p><a href="#">Githubアカウントへ</a></p>
   </div>
 </template>
